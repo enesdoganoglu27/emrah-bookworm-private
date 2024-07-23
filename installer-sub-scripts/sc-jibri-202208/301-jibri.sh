333ceb5a1b1a1706108168f22ec0460d85445595
enesdoganoglu27 <enesjava1@gmail.com>
2024-07-23 15:04:51 +0300

    Update 301-jibri.sh

--- installer-sub-scripts/sc-jibri-202208/301-jibri.sh
+++ installer-sub-scripts/sc-jibri-202208/301-jibri.sh
@@ -25,8 +25,8 @@ echo "-------------------------- $MACH --------------------------"
 # ------------------------------------------------------------------------------
 # stop the template container if it's running
 set +e
-lxc-stop -n eb-trixie
-lxc-wait -n eb-trixie -s STOPPED
+lxc-stop -n eb-bookworm
+lxc-wait -n eb-bookworm -s STOPPED
 set -e
 
 # remove the old container if exists
@@ -41,7 +41,7 @@ sleep 1
 set -e
 
 # create the new one
-lxc-copy -n eb-trixie -N $MACH -p /var/lib/lxc/
+lxc-copy -n eb-bookworm -N $MACH -p /var/lib/lxc/
 
 # the shared directories
 mkdir -p $SHARED/cache
