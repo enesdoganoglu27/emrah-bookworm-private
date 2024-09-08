9e8336b2ea3109d2d2923baff5bd5387651eb358
enesdoganoglu0627 <enes_doganoglu0627@hotmail.com>
2024-09-08 16:07:53 +0300

    Update 302-jitsi.sh

--- installer-sub-scripts/sc-jitsi/302-jitsi.sh
+++ installer-sub-scripts/sc-jitsi/302-jitsi.sh
@@ -72,8 +72,8 @@ fi
 # ------------------------------------------------------------------------------
 # stop the template container if it's running
 set +e
-lxc-stop -n eb-bullseye
-lxc-wait -n eb-bullseye -s STOPPED
+lxc-stop -n eb-bookworm
+lxc-wait -n eb-bookworm -s STOPPED
 set -e
 
 # remove the old container if exists
@@ -86,7 +86,7 @@ sleep 1
 set -e
 
 # create the new one
-lxc-copy -n eb-bullseye -N $MACH -p /var/lib/lxc/
+lxc-copy -n eb-bookworm -N $MACH -p /var/lib/lxc/
 
 # the shared directories
 mkdir -p $SHARED/cache
@@ -181,7 +181,7 @@ debconf-set-selections <<< \
 debconf-set-selections <<< \
     'jitsi-meet-web-config jitsi-meet/cert-choice select Generate a new self-signed certificate'
 
-apt-get $APT_PROXY -y install openjdk-11-jre-headless
+apt-get $APT_PROXY -y install openjdk-17-jre-headless
 apt-get $APT_PROXY -y --install-recommends install jitsi-meet
 EOS
 
